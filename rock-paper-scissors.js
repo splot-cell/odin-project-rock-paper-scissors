d50f14dccf54bd447e389b04033f49f9353ec050
Olly Seber <olly_s@hotmail.co.uk>
2022-06-03 20:22:50 -0700

    Add welcome message

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,18 @@
 // Print welcome message
+function printWelcome() {
+    console.log(
+`---WELCOME TO ROCK PAPER SCISSORS---
+   The hottest new game in town!
+        
+Instructions:
+Make a selection of either "rock", "paper", or "scissors", by typing it when prompted.
+Try and beat the highly advanced computer AI.
+
+Good luck!
+`   )
+}
+// printWelcome();
+
 // Generate computer's choice
 // Ask user for their choice
 //      Check the user selected "rock", "paper", or "scissors"
