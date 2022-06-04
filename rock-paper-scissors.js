fa859ed43955442f00a83a407399231a9e3ba6fa
Olly Seber <olly_s@hotmail.co.uk>
2022-06-03 20:11:52 -0700

    Add pseudocode to js file

---
+++ rock-paper-scissors.js
@@ -0,0 +1,10 @@
+// Print welcome message
+// Generate computer's choice
+// Ask user for their choice
+//      Check the user selected "rock", "paper", or "scissors"
+//      If not, ask them for their choice again
+// Compare the user's choice and the computer's choice
+// Print result of comparison
+//      If it's a draw, play again
+// Add scores to counter
+// Once 5 rounds are played, print results
