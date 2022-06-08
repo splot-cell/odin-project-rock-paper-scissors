a1697eb237119d2520f0d2854a9cf7ac71b6d676
Olly Seber <olly_s@hotmail.co.uk>
2022-06-08 12:48:04 -0700

    Refactor comments and function order

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,68 +1,17 @@
-// Randomly generate computer's choice
-function getCompChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    let choiceString;
-    switch (choice) {
-        case 0:
-            choiceString = "rock";
-            break;
-        case 1:
-            choiceString = "paper";
-            break;
-        case 2:
-            choiceString = "scissors";
-            break;
-        default:
-            choiceString = "rock";
-    }
-    return choiceString;
-}
-
-function displayResult(string) {
-    const resultsDiv = document.getElementById("results");
-    resultsDiv.textContent = string;
-}
-
-function resetScores(e) {
-    const compScore = document.getElementById("comp-score");
-    compScore.textContent = "0";
-    const userScore = document.getElementById("user-score");
-    userScore.textContent = "0";
-    e.target.style.visibility = "hidden";
+// Add event listeners to all buttons
+function setup() {
     enableUserChoiceButtons();
-}
 
-function showResetButton() {
     const resetButton = document.getElementById("reset-button");
-    resetButton.style.visibility = "visible";
+    resetButton.addEventListener("click", resetScores);
 }
 
-function updateScore(winner) {
-    if (winner === "comp") {
-        const compScore = document.getElementById("comp-score");
-        let currentScore = Number(compScore.textContent) + 1;
-        compScore.textContent = currentScore.toString();
-        if (currentScore === 5) {
-            disableUserChoiceButtons();
-            displayResult("COMPUTER WINS!");
-            showResetButton();
-        }
-    } else { // winner == "user"
-        const userScore = document.getElementById("user-score");
-        let currentScore = Number(userScore.textContent) + 1;
-        userScore.textContent = currentScore.toString();
-        if (currentScore === 5) {
-            disableUserChoiceButtons();
-            displayResult("YOU WIN!");
-            showResetButton();
-        }
-    }
-}
+setup();
 
-// Compare the user's choice and the computer's choice
 function playRound(e) {
     let userChoice = e.target.id;
     let compChoice = getCompChoice();
+    // Determine winner and display results
     if(userChoice === compChoice) {
         displayResult(`It's a draw this time, choose again...`);
     }
@@ -92,6 +41,67 @@ function playRound(e) {
     }
 }
 
+// Randomly generate computer's choice
+function getCompChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    let choiceString;
+    switch (choice) {
+        case 0:
+            choiceString = "rock";
+            break;
+        case 1:
+            choiceString = "paper";
+            break;
+        case 2:
+            choiceString = "scissors";
+            break;
+        default:
+            choiceString = "rock";
+    }
+    return choiceString;
+}
+
+function displayResult(string) {
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.textContent = string;
+}
+
+function updateScore(winner) {
+    if (winner === "comp") {
+        const compScore = document.getElementById("comp-score");
+        let currentScore = Number(compScore.textContent) + 1;
+        compScore.textContent = currentScore.toString();
+        if (currentScore === 5) {
+            disableUserChoiceButtons();
+            displayResult("COMPUTER WINS!");
+            showResetButton();
+        }
+    } else { // winner === "user"
+        const userScore = document.getElementById("user-score");
+        let currentScore = Number(userScore.textContent) + 1;
+        userScore.textContent = currentScore.toString();
+        if (currentScore === 5) {
+            disableUserChoiceButtons();
+            displayResult("YOU WIN!");
+            showResetButton();
+        }
+    }
+}
+
+function resetScores(e) {
+    const compScore = document.getElementById("comp-score");
+    compScore.textContent = "0";
+    const userScore = document.getElementById("user-score");
+    userScore.textContent = "0";
+    e.target.style.visibility = "hidden";
+    enableUserChoiceButtons();
+}
+
+function showResetButton() {
+    const resetButton = document.getElementById("reset-button");
+    resetButton.style.visibility = "visible";
+}
+
 function enableUserChoiceButtons() {
     const rockButton = document.getElementById("rock");
     const paperButton = document.getElementById("paper");
@@ -111,12 +121,3 @@ function disableUserChoiceButtons() {
     paperButton.removeEventListener("click", playRound);
     scissorsButton.removeEventListener("click", playRound);
 }
-
-function setup() {
-    enableUserChoiceButtons();
-
-    const resetButton = document.getElementById("reset-button");
-    resetButton.addEventListener("click", resetScores);
-}
-
-setup();
