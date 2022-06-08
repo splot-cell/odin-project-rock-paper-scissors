de712c42fe2071780c2444a812807dfb707059b8
Olly Seber <olly_s@hotmail.co.uk>
2022-06-08 11:43:20 -0700

    Display and update scores on webpage

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,4 @@
-// Generate computer's choice
+// Randomly generate computer's choice
 function getCompChoice() {
     let choice = Math.floor(Math.random() * 3);
     let choiceString;
@@ -23,38 +23,48 @@ function displayResult(string) {
     resultsDiv.textContent = string;
 }
 
+function updateScore(winner) {
+    if (winner === "comp") {
+        const compScore = document.getElementById("comp-score");
+        let currentScore = Number(compScore.textContent) + 1;
+        compScore.textContent = currentScore.toString();
+    } else if (winner === "user") {
+        const userScore = document.getElementById("user-score");
+        let currentScore = Number(userScore.textContent) + 1;
+        userScore.textContent = currentScore.toString();
+    }
+}
+
 // Compare the user's choice and the computer's choice
 function playRound(userChoice) {
     let compChoice = getCompChoice();
-    // Print result of comparison
     if(userChoice === compChoice) {
-        // If it's a draw
         displayResult(`It's a draw this time, choose again...`);
-        return null;
+        updateScore(null);
     }
     if (userChoice === `rock` && compChoice === `paper`) {
         displayResult("You lose! Paper beats rock.");
-        return "comp";
+        updateScore("comp");
     }
     if (userChoice === `rock` && compChoice === `scissors`) {
         displayResult("You win! Rock beats scissors.");
-        return "user";
+        updateScore("user");
     }
     if (userChoice === `paper` && compChoice === `rock`) {
         displayResult("You win! Paper beats rock.");
-        return "user";
+        updateScore("user");
     }
     if (userChoice === `paper` && compChoice === `scissors`) {
         displayResult("You lose! Scissors beats paper.");
-        return "comp";
+        updateScore("comp");
     }
     if (userChoice === `scissors` && compChoice === `paper`) {
         displayResult("You win! Scissors beats paper.");
-        return "user";
+        updateScore("user");
     }
     if (userChoice === `scissors` && compChoice === `rock`) {
         displayResult("You lose! Rock beats scissors.");
-        return "comp";
+        updateScore("comp");
     }
 }
 
